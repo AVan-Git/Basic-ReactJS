d5bc9a3479952e48c919d3597fb753e149ffa13c
AVan-Git <hoangkc35@gmail.com>
2023-09-01 11:15:45 +0700

    start buoi 4 : sự kiện

---
+++ demo-buoi4-event/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import DemoXemChiTiet from './Props/DemoXemChiTiet/DemoXemChiTiet';
+
+function App() {
+  return (
+    <>
+      <DemoXemChiTiet />
+    </>
+  );
+}
+
+export default App;
