5b80d1084373db4903b704b83e3336fc02f2d18b
AVan-Git <hoangkc35@gmail.com>
2023-09-05 09:40:24 +0700

    screen Detail: up data

--- demo-buoi4-event/src/Props/DataBase/DataPhone.js
+++ demo-buoi4-event/src/Props/DataBase/DataPhone.js
@@ -1,5 +1,7 @@
 const data = [
-    {"maSP": 1, "tenSP": "VinSmart Live", "manHinh": "AMOLED, 6.2, Full HD+", "heDieuHanh": "Android 9.0 (Pie)", "cameraTruoc": "20 Mp", "cameraSau": "Chính 48 MP & Phụ 8 MP, 5 MP", "ram": "4 GB", "rom": "64 GB", "giaBan": 5700000, "hìnhAnh": "./img/vsmart.jpg" },
+    {"maSP": 1, "tenSP": "VinSmart Live", "manHinh": "AMOLED, 6.2, Full HD+", "heDieuHanh": "Android 9.0 (Pie)", "cameraTruoc": "20 Mp", "cameraSau": "Chính 48 MP & Phụ 8 MP, 5 MP", "ram": "4 GB", "rom": "64 GB", "giaBan": 5700000, "hinhAnh": "./img/vsmart.webp" },
     {"maSP": 2, "tenSP": "Meizu 16Xs",    "manHinh": "AMOLED, FHD+ 2732 x 1080 pixels", "heDieuHanh": "Android 9.0 (Pie); Flyme", "cameraTruoc": "20 MP", "cameraSau": "Chính 13 MP & Phụ 8 MP, 5 Mp", "ram": "4 GB" , "rom": "64 GB", "giaBan": 7600000, "hinhAnh": "./img/samsung.webp"},
     {"maSP": 3, "tenSP": "Iphone XS Max", "manHinh": "OLED, 6.5, 1242 x 2688 Pixels", "heDieuHanh": "Android 10s", "cameraSau": "Chính 12 MP & Phụ 12 Mp", "cameraTruoc": "7 MP", "ram": "4 GB", "rom": "64 GB", "giaBan": 27000000, "hinhAnh": "./img/iphone.webp" }
 ]
+
+export default data
--- demo-buoi4-event/src/Props/DemoXemChiTiet/DemoXemChiTiet.jsx
+++ demo-buoi4-event/src/Props/DemoXemChiTiet/DemoXemChiTiet.jsx
@@ -1,11 +1,89 @@
 import React, { Component } from 'react'
+import DataPhone from '../DataBase/DataPhone'
+
+
+const data = DataPhone;
 
 export default class DemoXemChiTiet extends Component {
-  render() {
-    return (
-      <div>
-        DemoXemChiTiet
-      </div>
-    )
-  }
+
+   
+
+   renderProduct = () => {
+         return data.map((item, index) => {
+            console.log("🚀 ~ file: DemoXemChiTiet.jsx:12 ~ DemoXemChiTiet ~ returndata.map ~ item:", item)
+            return (
+               <div className="col-4 mt-3" key={index}>
+                  <div className="card" style={{ backgroundColor: "rgba(84, 231, 247)", borderColor: 'darkblue' }}>
+                     <img className="card-img-top" src={item.hinhAnh} alt="Title" />
+                     <div className="card-body">
+                        <h4 className="card-title">{item.tenSP}</h4>
+                        <p className="card-text">{item.giaBan} VND</p>
+
+                        <button type="button" class="btn btn-primary" data-bs-toggle="button" aria-pressed="false" autocomplete="off">Xem Chi Tiết</button>
+                     </div>
+                  </div>
+               </div>
+            )
+         })
+      }
+   render() {
+
+      
+
+      return (
+         <div className='container mt-3'>
+            <h2 className="text-center text-danger">DemoXemChiTiet</h2>
+            <hr />
+
+            <div className="row">
+
+               {this.renderProduct()}
+
+            </div>
+
+            {/* Detail Phone */}
+
+            <div className="detail-phone mt-2">
+               <div className="row">
+                  <div className="col-3">
+                     <h3 className="text-center">Iphone</h3>
+                     <img className="card-img-top" src="./img/samsung.webp" alt="Title" />
+                  </div>
+                  <div className="col-9">
+                     <h3 className="text-center">Thông tin chi tiết</h3>
+                     <table className='table'>
+                        <tbody>
+                           <tr>
+                              <td>Màn Hình</td>
+                              <td>Màn Hình</td>
+                           </tr>
+                           <tr>
+                              <td>Hệ điều hành</td>
+                              <td>Hệ điều hành</td>
+                           </tr>
+                           <tr>
+                              <td>Camera trước</td>
+                              <td>Camera trước</td>
+                           </tr>
+                           <tr>
+                              <td>Camera Sau</td>
+                              <td>Camera Sau</td>
+                           </tr>
+                           <tr>
+                              <td>Ram</td>
+                              <td>Ram</td>
+                           </tr>
+                           <tr>
+                              <td>Rom</td>
+                              <td>Rom</td>
+                           </tr>
+                        </tbody>
+                     </table>
+                  </div>
+               </div>
+            </div>
+
+         </div>
+      )
+   }
 }
