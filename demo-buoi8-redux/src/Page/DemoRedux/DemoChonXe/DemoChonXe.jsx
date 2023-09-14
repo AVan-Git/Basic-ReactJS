7dc9ec8becda2c29ed500084fe8cc556ff17d3ae
AVan-Git <hoangkc35@gmail.com>
2023-09-14 10:33:05 +0700

    finish BT chon xe

--- demo-buoi8-redux/src/Page/DemoRedux/DemoChonXe/DemoChonXe.jsx
+++ demo-buoi8-redux/src/Page/DemoRedux/DemoChonXe/DemoChonXe.jsx
@@ -1,40 +1,57 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 class DemoChonXe extends Component {
   static propTypes = {
-    prop: PropTypes
-  }
+    prop: PropTypes,
+  };
+
+  bandleCar = (color) => {
+    // buoc 2: viet hàm gửi datalen redux
+    const action = {
+      type: color,
+    };
+    this.props.dispatch(action);
+  };
 
   render() {
+    // console.log(this.props);
+
     return (
-      <div className='container'>
+      <div className="container">
         <h3 className="text-center text-danger">Bài tập chọn xe</h3>
         <hr />
         <div className="row">
-            <div className="col-6">
-                <img src="./img/products/car_black.webp" alt="my car" className='w-100' />
+          <div className="col-6">
+            <img src={this.props.img} alt="my car" className="w-100" />
+          </div>
+          <div className="col-6">
+            <div className="mx-2">
+              <button 
+                onClick={() => this.bandleCar("white")}
+                class="btn btn-white mx-2"
+                style={{ border: "1", borderColor: "black" }}
+              >
+                White
+              </button>
+              <button className="btn btn-dark mx-2"
+              onClick={() => this.bandleCar("black")}> Black</button>
+              <button className="btn btn-primary mx-2"
+              onClick={() => this.bandleCar("blue")}> Blue</button>
+              <button className="btn btn-warning mx-2"
+              onClick={() => this.bandleCar("yellow")}> Yellow</button>
             </div>
-            <div className="col-6">
-                <div className="mx-2">
-                    <button  class="btn btn-white mx-2" style={{"border": "1", borderColor:'black'}}>White</button>
-                    <button className="btn btn-dark mx-2"> Black</button>
-                    <button className="btn btn-primary mx-2"> Blue</button>
-                    <button className="btn btn-warning mx-2"> Yellow</button>
-                </div>
-            </div>
+          </div>
         </div>
-        
       </div>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state) => ({
-  
+  //buoc 1: khongo can return vi ham viết tắt ({})
+  img: state.stateCar,
+});
 
-    
-})
-
-export default connect(mapStateToProps )(DemoChonXe)
+export default connect(mapStateToProps)(DemoChonXe);
--- demo-buoi8-redux/src/Redux/ConfigStore.jsx
+++ demo-buoi8-redux/src/Redux/ConfigStore.jsx
@@ -1,28 +1,45 @@
-import {createStore, combineReducers} from 'redux'
+import { createStore, combineReducers } from "redux";
 
 const rootReducer = combineReducers({
-    // Nơi chứa các state của ứng dụng
+  // Nơi chứa các state của ứng dụng
 
-    number: (state = 12, action) => {
-        switch (action.type) {
-            case "TANG_SL":
-                //b1: lay gia tri tu  payload len
-                const {payload} = action;
-                // b2: thay doi state
-                state += payload;
-                // b3 return ve state moi
-                return state;
-        
-            default:
-                break;
-        }
+  number: (state = 12, action) => {
+    switch (action.type) {
+      case "TANG_SL":
+        //b1: lay gia tri tu  payload len
+        const { payload } = action;
+        // b2: thay doi state
+        state += payload;
+        // b3 return ve state moi
         return state;
-    },
-    stateCar: (state = "./img/products/car_black.webp", action) => {
-        return state;
+
+      default:
+        break;
     }
+    return state;
+  },
+  stateCar: (state = "./img/products/car_black.webp", action) => {
+    // buoc 3: nhận dât và cập nhật
+    switch (action.type) {
+      case "white":
+        state = "./img/products/car_white.webp";
+        break;
+      case "yellow":
+        state = "./img/products/car_yellow.webp";
+        break;
+      case "blue":
+        state = "./img/products/car_blue.webp";
+        break;
 
+      default:
+        state = "./img/products/car_black.webp";
+        break;
+    }
+    return state;
+  },
+});
 
-})
-
-export const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
