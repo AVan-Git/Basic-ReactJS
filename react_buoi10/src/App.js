b24e74d28c8f9dd9d4272b7c584230ba907dd87c
AVan-Git <hoangkc35@gmail.com>
2023-09-20 15:55:35 +0700

    add 1 navbar

--- react_buoi10/src/App.js
+++ react_buoi10/src/App.js
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import HearHome from "./components/HearHome";
 
 function App() {
   return (
     <div className="App">
+      <header>
+        <HearHome />
+      </header>
       <Outlet />
     </div>
   );
--- react_buoi10/src/pages/UseEffectDemo/UseEffectDemo.jsx
+++ react_buoi10/src/pages/UseEffectDemo/UseEffectDemo.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function UseEffectDemo() {
   return (
     <div>
-      
+      UseEffectDemo
     </div>
   )
 }
