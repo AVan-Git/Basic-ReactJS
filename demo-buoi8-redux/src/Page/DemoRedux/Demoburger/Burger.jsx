cc8a9771a5ec27dc190dafbb69800f647849a8bf
AVan-Git <hoangkc35@gmail.com>
2023-09-20 10:26:23 +0700

    load data redux -> component burger

--- demo-buoi8-redux/src/Page/DemoRedux/Demoburger/Burger.jsx
+++ demo-buoi8-redux/src/Page/DemoRedux/Demoburger/Burger.jsx
@@ -7,12 +7,30 @@ class Burger extends Component {
     prop: PropTypes,
   };
 
+  renderTopping = () => {
+    let {burger} = this.props;
+    console.log("🚀 ~ file: Burger.jsx:12 ~ Burger ~ burger:", burger)
+    const arrDivJsx = []
+    burger.forEach(item => {
+        let {quantity} = item;
+        for (let index = 0; index < quantity; index++) {
+            const divTopping = <div className={item.id} key={`${item.id}_${quantity}`}></div>
+            arrDivJsx.push(divTopping)
+            
+        }
+    });
+
+    return arrDivJsx;
+  }
+
   render() {
+    
     return <>
         <div className="breadTop"></div>
-        <div className="salad"></div>
+        {/* <div className="salad"></div>
         <div className="beef"></div>
-        <div className="cheese"></div>
+        <div className="cheese"></div> */}
+        {this.renderTopping()}
         <div className="breadBottom"></div>
     </>;
   }
