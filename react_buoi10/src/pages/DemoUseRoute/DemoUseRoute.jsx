5af3bda38fa5c17823618cef4684b3fa564f7cbf
AVan-Git <hoangkc35@gmail.com>
2023-10-10 16:07:15 +0700

    text cookie -- khong tự load

--- react_buoi10/src/pages/DemoUseRoute/DemoUseRoute.jsx
+++ react_buoi10/src/pages/DemoUseRoute/DemoUseRoute.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import useRoute from '../../hooks/useRoute'
+import React, { useEffect, useState } from "react";
+import useRoute from "../../hooks/useRoute";
+import { useCookie } from "react-use";
 
 export default function DemoUseRoute() {
+  const {
+    navigate,
+    params,
+    searchParams: [searchParams, setSearchParams],
+  } = useRoute();
 
-    const {navigate, params, searchParams:[searchParams, setSearchParams] } = useRoute()
+  const [value, updateCookie, deleteCookie] = useCookie("my-cookie");
+  const [counter, setCounter] = useState(1);
 
-    console.log({navigate, params, searchParams:[searchParams, setSearchParams] });
+  useEffect(() => {
+    deleteCookie();
+  }, []);
+
+  const updateCookieHandler = () => {
+    updateCookie(`my-awesome-cookie-${counter}`);
+    setCounter((c) => c + 1);
+  };
   return (
     <div>
-      
+      <p>Value: {value}</p>
+      <button onClick={updateCookieHandler}>Update Cookie</button>
+      <br />
+      <button onClick={deleteCookie}>Delete Cookie</button>
     </div>
-  )
+  );
 }
