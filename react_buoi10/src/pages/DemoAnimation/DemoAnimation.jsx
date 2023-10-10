66b9e46acc8efb94aeb4e765330cdb41f2eae671
AVan-Git <hoangkc35@gmail.com>
2023-10-10 17:21:33 +0700

    update animation

--- react_buoi10/src/pages/DemoAnimation/DemoAnimation.jsx
+++ react_buoi10/src/pages/DemoAnimation/DemoAnimation.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { animated, useSpring } from "react-spring";
 
 export default function DemoAnimation() {
   let [toggle, setToggle] = useState(true);
 
-  const [props, api] = useSpring(() => ({ opacity: 1, color:'red' }));
+  const [props, api] = useSpring(() => ({ opacity: 1, color:'red' , fontSize: 20  }));
   // Update spring with new props
 //   api.start({ opacity: 0, color:'blue' });
   // Stop animation
   //   api.stop();
+
+  useEffect(() => {
+    return () => { // animation luon hoat dong khi doi trang len phair stop() no
+        api.stop()
+    }
+    
+  },[])
 
   return (
     <div className="container">
@@ -16,9 +23,9 @@ export default function DemoAnimation() {
         type="button"
         onClick={() => {
             if (toggle) {
-                api.start({ opacity: 0, color:'blue' });
+                api.start({ opacity: 0, color:'blue', fontSize: 10 });
             }else {
-                api.start({ opacity: 1, color:'red' });
+                api.start({ opacity: 1, color:'red' , fontSize: 30 });
             }
 
           setToggle(!toggle);
