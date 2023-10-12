e77313a72ca0d77fddd3ae06ca226705727d4e2c
AVan-Git <hoangkc35@gmail.com>
2023-10-12 15:25:34 +0700

    HOC cach 2 - truyen thong quar props

--- react_buoi10/src/pages/DemoHOC/DemoHOC.jsx
+++ react_buoi10/src/pages/DemoHOC/DemoHOC.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Login from '../Login/Login'
 import ModelHOC from '../../HOC/ModelHOC'
 import UseStateDemo from '../UseStateDemo/UseStateDemo'
+import HOCModel from '../../HOC/HOCModel'
 
 
 //tao component tu HOC
@@ -13,8 +14,12 @@ export default function DemoHOC() {
     <div>
       DemoHOC
 
-      <WrapFormLoginModal />
-      <WrapFormUseStateDemoModal />
+      {/* <WrapFormLoginModal />
+      <WrapFormUseStateDemoModal /> */}
+
+     {/* HOC cach 2 */}
+     <HOCModel Component={Login} title={<h1 className='text-danger'>Login</h1>} id="modalId1"/>
+     <HOCModel Component={UseStateDemo} id="modalId2"/>
     </div>
   )
 }
